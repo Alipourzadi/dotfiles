52fa9d8483b0418b9a9ce5bc8b74eb0b37c7fbc0
Ali Pourzadi <ali.pourzadii@gmail.com>
2025-06-10 17:26:06 +0330

    chore(wezterm): change font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,17 +5,17 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha (Gogh)"
 
 config.font_size = 14
-config.line_height = 1.1
+config.line_height = 1.2
 config.font = wezterm.font_with_fallback({
-  "JetBrainsMonoNL NFP",
-  "Vazirmatn",
+	"FiraCode Nerd Font Propo",
+	"Vazirmatn",
 })
 
 config.window_padding = {
-  left = 8,
-  right = 8,
-  top = 8,
-  bottom = 8,
+	left = 8,
+	right = 8,
+	top = 8,
+	bottom = 8,
 }
 
 config.animation_fps = 240
