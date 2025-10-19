b84a98574b4b302e291ae981aaccd093a6a38f81
Ali Pourzadi <ali.pourzadii@gmail.com>
2025-10-20 01:28:15 +0330

    chore(nvim): ditch unused plugins

--- nvim/.config/nvim/lua/plugins/typescript-tools.lua
+++ nvim/.config/nvim/lua/plugins/typescript-tools.lua
@@ -1,6 +1,7 @@
 return {
 	"pmizio/typescript-tools.nvim",
 	requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+	enabled = false,
 	config = function()
 		require("typescript-tools").setup({
 			settings = {
